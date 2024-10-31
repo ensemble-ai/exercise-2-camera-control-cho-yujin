f2d10149cf8afba1f0328e64045921e10d3ad14c
Yujin Cho <yujincho2002@gmail.com>
2024-10-30 23:00:54 -0700

    stage 1 complete

--- Obscura/scripts/camera_controllers/horizontal_auto_scroll.gd
+++ Obscura/scripts/camera_controllers/horizontal_auto_scroll.gd
@@ -4,9 +4,19 @@ extends CameraControllerBase
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	super()
+	position = target.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if !current:
+		return
+		
+	if draw_camera_logic:
+		draw_logic()
+		
+	super(delta)
+
+func draw_logic() -> void:
 	pass
--- Obscura/scripts/camera_controllers/position_lock.gd
+++ Obscura/scripts/camera_controllers/position_lock.gd
@@ -10,4 +10,42 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if !current:
+		return
+		
+	if draw_camera_logic:
+		draw_logic()
+	
+	global_position.x = target.global_position.x
+	global_position.z = target.global_position.z
+	
+	super(delta)
+
+func draw_logic() -> void:
+	var mesh_instance := MeshInstance3D.new()
+	var immediate_mesh := ImmediateMesh.new()
+	var material := ORMMaterial3D.new()
+	
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(Vector3(5, 0, 0))
+	immediate_mesh.surface_add_vertex(Vector3(-5, 0, 0))
+	immediate_mesh.surface_end()
+	
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, 5))
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, -5))
+	immediate_mesh.surface_end()
+	
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = Color.WHITE
+	
+	add_child(mesh_instance)
+	mesh_instance.global_transform = Transform3D.IDENTITY
+	mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
+
+	await get_tree().process_frame
+	mesh_instance.queue_free()
+	
